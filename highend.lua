f8566d2d135adba3edaf4f9bcfdab77e16953eb4
Hycial <129767417+Hycial@users.noreply.github.com>
2023-04-14 16:32:45 +0300

    Update highend.lua

--- highend.lua
+++ highend.lua
@@ -11,6 +11,42 @@ local TextButton = Instance.new("TextButton")
 
 --Properties:
 
+Toggle.Name = "Toggle"
+Toggle.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Toggle.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Toggle.DisplayOrder = 999999998
+
+Frame.Parent = Toggle
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0.500
+Frame.Position = UDim2.new(0.846244156, 0, 0.0386473425, 0)
+Frame.Size = UDim2.new(0.131455392, 0, 0.270531386, 0)
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Size = UDim2.new(1, 0, 1, 0)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Pwnerhub"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextSize = 30.000
+TextButton.MouseButton1Down:Connect(function()
+	game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
+end)
+
+P = game:GetService("Players").LocalPlayer
+P.CharacterAdded:Connect(function(char)
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Toggle = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
 TextButton.Parent = Frame TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255) TextButton.BackgroundTransparency = 1.000 TextButton.Size = UDim2.new(1, 0, 1, 0) TextButton.Font = Enum.Font.SourceSans TextButton.Text = "HiScript" TextButton.TextColor3 = Color3.fromRGB(0, 0, 0) TextButton.TextSize = 30.000 TextButton.MouseButton1Down:Connect(function() game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game) end)
 
 Toggle.Name = "Toggle"
